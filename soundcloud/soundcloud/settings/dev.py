a1bae0f22a3252886a4c32cbc731fc747788e648
Jaewan Park <jaejae1112@gmail.com>
2021-11-23 22:55:58 +0900

    Feat: Start django app

---
+++ soundcloud/soundcloud/settings/dev.py
@@ -0,0 +1,26 @@
+from SoundCloud.settings.common import *
+
+DEBUG = True
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+    INTERNAL_IPS = ['127.0.0.1', ]
+
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'waffle-team-10-rds.cf6av8nj1vlu.ap-northeast-2.rds.amazonaws.com',
+        'PORT': 3306,
+        'NAME': 'soundcloud',
+        'USER': 'waffle-team-10',
+        'PASSWORD': get_secret("DB_PASSWORD"),
+    }
+}
---
+++ soundcloud/soundcloud/settings/prod.py
@@ -0,0 +1,29 @@
+from SoundCloud.settings.common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+    INTERNAL_IPS = ['127.0.0.1', ]
+
+
+ALLOWED_HOSTS = [
+    '3.36.52.154',
+]
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'waffle-team-10-rds.cf6av8nj1vlu.ap-northeast-2.rds.amazonaws.com',
+        'PORT': 3306,
+        'NAME': 'soundcloud',
+        'USER': 'waffle-team-10',
+        'PASSWORD': get_secret("DB_PASSWORD"),
+    }
+}
