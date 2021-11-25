014b0e4a30d36dc9178161358afe53d9d397c0ea
askrid <joonwoo3023@gmail.com>
2021-11-26 03:28:37 +0900

    Fix: birthday field defalt value

--- soundcloud/user/models.py
+++ soundcloud/user/models.py
@@ -25,7 +25,7 @@ class User(AbstractBaseUser):
     display_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    birthday = models.DateField(default=date.today())
+    birthday = models.DateField(default=date.today)
     gender = models.CharField(max_length=20, blank=True)
     first_name = models.CharField(max_length=35, blank=True)
     last_name = models.CharField(max_length=35, blank=True)
