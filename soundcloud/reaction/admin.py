fd67e0e1b93941aefc3cb0880d0b1ec09f988401
askrid <joonwoo3023@gmail.com>
2021-12-17 16:31:38 +0900

    Feature: admin site register

--- soundcloud/reaction/admin.py
+++ soundcloud/reaction/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from reaction.models import *
 
 # Register your models here.
+admin.site.register(Like)
+admin.site.register(Repost)
--- soundcloud/set/admin.py
+++ soundcloud/set/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from set.models import *
 
 # Register your models here.
+admin.site.register(Set)
