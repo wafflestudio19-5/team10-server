b83df3b29443e2c231a98e0af529ac8a2fff1f8f
askrid <joonwoo3023@gmail.com>
2021-12-23 09:41:31 +0900

    Fix: reaction models limit

--- soundcloud/reaction/models.py
+++ soundcloud/reaction/models.py
@@ -3,16 +3,21 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+reaction_limit = models.Q(app_label='track', model='Track') | models.Q(app_label='set', model='Set')
+
 class Like(models.Model):
+
     user = models.ForeignKey(get_user_model(), related_name="likes", on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, limit_choices_to=reaction_limit, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Repost(models.Model):
+
     user = models.ForeignKey(get_user_model(), related_name="reposts", on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, limit_choices_to=reaction_limit, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
