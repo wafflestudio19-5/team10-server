5a7bfa8c1cb3c0e0f04ee849c86534382dced92d
bsm2002 <gggshhsl@gmail.com>
2021-12-26 18:02:35 +0900

    Fix: typo

--- soundcloud/reaction/urls.py
+++ soundcloud/reaction/urls.py
@@ -5,6 +5,6 @@ from .views import LikeTrackAPIView, LikeSetAPIView, RepostSetAPIView, RepostTra
 urlpatterns = [
     path('likes/tracks/<str:track_id>', LikeTrackAPIView.as_view(), name='like-track'),  # /likes
     path('likes/sets/<str:set_id>', LikeSetAPIView.as_view(), name='like-set'),
-    path('reposts/tracks/<str:pk>', RepostTrackAPIView.as_view(), name='repost-track'),  # /reposts
-    path('reposts/sets/<str:pk>', RepostSetAPIView.as_view(), name='repost-set'),
+    path('reposts/tracks/<str:track_id>', RepostTrackAPIView.as_view(), name='repost-track'),  # /reposts
+    path('reposts/sets/<str:set_id>', RepostSetAPIView.as_view(), name='repost-set'),
 ]
