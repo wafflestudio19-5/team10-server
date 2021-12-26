bc44c60a92d8db1f9fc929dbfc34e128342cc0bc
bsm2002 <gggshhsl@gmail.com>
2021-12-26 17:43:40 +0900

    Fix: reaction views

---
+++ soundcloud/reaction/serializers.py
@@ -0,0 +1,82 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework import serializers
+from track.models import Track
+from set.models import Set
+from reaction.models import Like, Repost
+from soundcloud.exceptions import ConflictError
+from rest_framework import status
+
+class LikeService(serializers.Serializer):
+
+    def execute(self):
+        type = self.context.get('type')
+        id = self.context.get('id')
+        method = self.context.get('method')
+        user = self.context.get('user')
+
+        if type == 'track':
+            track = get_object_or_404(Track, id=id)
+            if method == 'POST':
+                try:
+                    Like.objects.get(track__id=track.id, user=user)
+                    raise ConflictError("Already liked")
+                except Like.DoesNotExist:
+                    Like.objects.create(content_object=track, user=user)
+                    return status.HTTP_201_CREATED, 'Liked'
+            elif method == 'DELETE':
+                like = get_object_or_404(Like, track__id=track.id, user=user)
+                like.delete()
+
+                return status.HTTP_200_OK, 'Unliked'
+        elif type == 'set':
+            set = get_object_or_404(Set, id=id)
+            if method == 'POST':
+                try:
+                    Like.objects.get(set__id=set.id, user=user)
+                    raise ConflictError("Already liked")
+                except Like.DoesNotExist:
+                    Like.objects.create(content_object=set, user=user)
+                    return status.HTTP_201_CREATED, 'Liked'
+            elif method == 'DELETE':
+                like = get_object_or_404(Like, set__id=set.id, user=user)
+                like.delete()
+
+                return status.HTTP_200_OK, 'Unliked'
+
+
+class RepostService(serializers.Serializer):
+
+    def execute(self):
+        type = self.context.get('type')
+        id = self.context.get('id')
+        method = self.context.get('method')
+        user = self.context.get('user')
+
+        if type == 'track':
+            track = get_object_or_404(Track, id=id)
+            if method == 'POST':
+                try:
+                    Repost.objects.get(track__id=track.id, user=user)
+                    raise ConflictError("Already reposted")
+                except Repost.DoesNotExist:
+                    Repost.objects.create(content_object=track, user=user)
+                    return status.HTTP_201_CREATED, 'Reposted'
+            elif method == 'DELETE':
+                repost = get_object_or_404(Repost, track__id=track.id, user=user)
+                repost.delete()
+
+                return status.HTTP_200_OK, 'Unreposted'
+        elif type == 'set':
+            set = get_object_or_404(Set, id=id)
+            if method == 'POST':
+                try:
+                    Repost.objects.get(set__id=set.id, user=user)
+                    raise ConflictError("Already reposted")
+                except Repost.DoesNotExist:
+                    Repost.objects.create(content_object=set, user=user)
+                    return status.HTTP_201_CREATED, 'Reposted'
+            elif method == 'DELETE':
+                repost = get_object_or_404(Repost, set__id=set.id, user=user)
+                repost.delete()
+
+                return status.HTTP_200_OK, 'Unreposted'
--- soundcloud/reaction/views.py
+++ soundcloud/reaction/views.py
@@ -1,58 +1,68 @@
-from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.generics import get_object_or_404, GenericAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
-from track.models import Track
-from set.models import Set
-from .models import Like, Repost
-from soundcloud.exceptions import ConflictError
+from serializers import LikeService, RepostService
 
 
 class LikeTrackAPIView(GenericAPIView):
 
-    queryset = Like.objects
     serializer_class = serializers.Serializer
     lookup_field = 'track_id'
 
     def post(self, request, track_id=None):
-        track = get_object_or_404(Track, id=track_id)
-
-        try:
-            self.get_queryset().get(track__id=track.id, user=request.user)
-            raise ConflictError("Already liked")
-        except Like.DoesNotExist:
-            self.get_queryset().create(content_object=track, user=request.user)
-            return Response(data='Liked', status=status.HTTP_201_CREATED)
+        service = LikeService(context={'user': request.user, 'type': 'track', 'method': 'POST', 'id': track_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
 
     def delete(self, request, track_id=None):
-        track = get_object_or_404(Track, id=track_id)
-        like = get_object_or_404(Like, track__id=track.id, user=request.user)
-        like.delete()
-
-        return Response(data='Unliked', status=status.HTTP_200_OK)
+        service = LikeService(context={'user': request.user, 'type': 'track', 'method': 'DELETE', 'id': track_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
 
 
 class LikeSetAPIView(GenericAPIView):
 
-    queryset = Like.objects
     serializer_class = serializers.Serializer
     lookup_field = 'set_id'
 
     def post(self, request, set_id=None):
-        set = get_object_or_404(Set, id=set_id)
-
-        try:
-            self.get_queryset().get(set__id=set.id, user=request.user)
-            raise ConflictError("Already liked")
-        except Like.DoesNotExist:
-            self.get_queryset().create(content_object=set, user=request.user)
-            return Response(data='Liked', status=status.HTTP_201_CREATED)
+        service = LikeService(context={'user': request.user, 'type': 'set', 'method': 'POST', 'id': set_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
 
     def delete(self, request, set_id=None):
-        set = get_object_or_404(Set, id=set_id)
-        like = get_object_or_404(Like, set__id=set.id, user=request.user)
-        like.delete()
+        service = LikeService(context={'user': request.user, 'type': 'set', 'method': 'DELETE', 'id': set_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
 
-        return Response(data='Unliked', status=status.HTTP_200_OK)
+
+class RepostTrackAPIView(GenericAPIView):
+
+    serializer_class = serializers.Serializer
+    lookup_field = 'track_id'
+
+    def post(self, request, track_id=None):
+        service = RepostService(context={'user': request.user, 'type': 'track', 'method': 'POST', 'id': track_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
+
+    def delete(self, request, track_id=None):
+        service = RepostService(context={'user': request.user, 'type': 'track', 'method': 'DELETE', 'id': track_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
+
+
+class RepostSetAPIView(GenericAPIView):
+
+    serializer_class = serializers.Serializer
+    lookup_field = 'track_id'
+
+    def post(self, request, set_id=None):
+        service = RepostService(context={'user': request.user, 'type': 'set', 'method': 'POST', 'id': set_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
+
+    def delete(self, request, set_id=None):
+        service = RepostService(context={'user': request.user, 'type': 'set', 'method': 'DELETE', 'id': set_id})
+        service_status, data = service.execute()
+        return Response(status=service_status, data=data)
