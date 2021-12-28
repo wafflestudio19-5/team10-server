6cdea94ddf9afffbee45213df0737c9b46cba615
askrid <joonwoo3023@gmail.com>
2021-12-28 17:42:00 +0900

    Fix: track update conflict error

--- soundcloud/track/serializers.py
+++ soundcloud/track/serializers.py
@@ -1,10 +1,10 @@
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.serializers import ValidationError
 from track.models import Track
-from user.serializers import UserSerializer
 from tag.serializers import TagSerializer
+from user.serializers import UserSerializer
 from reaction.serializers import LikeSerializer, RepostSerializer
 from django.conf import settings
 from soundcloud.exceptions import ConflictError
@@ -51,11 +51,17 @@ class TrackSerializer(serializers.ModelSerializer):
             # 'comments',
         )
         extra_kwargs = {
-            'audio': {'read_only': True},
-            'image': {'read_only': True},
-            'created_at': {'read_only': True},
-            'count': {'read_only': True},
+            'permalink': {
+                'max_length': 255,
+                'min_length': 3,
+            },
         }
+        read_only_fields = (
+            'audio',
+            'image',
+            'created_at',
+            'count',
+        )
 
     @extend_schema_field(OpenApiTypes.INT)
     def get_like_count(self, track):
@@ -68,6 +74,68 @@ class TrackSerializer(serializers.ModelSerializer):
     @extend_schema_field(OpenApiTypes.INT)
     def get_comment_count(self, track):
         return track.comment_set.count()
+
+    def validate_audio_filename(self, value):
+        pattern = re.compile('^[a-zA-Z0-9\/\!\-\_\.\*\'\(\)]+$')
+
+        if not value.lower().endswith(('.wav', '.flac', '.aiff', '.alac', 'mp3', '.aac', '.ogg', '.oga', '.mp4', '.mp2', '.m4a', '.3gp', '.3g2', '.mj2', '.amr', '.wma')):
+            raise ValidationError("Unsupported audio file extension.")
+        if not re.search(pattern, value):
+            raise ValidationError("Incorrect audio filename format.")
+
+        return value
+
+    def validate_image_filename(self, value):
+        pattern = re.compile('^[a-zA-Z0-9\/\!\-\_\.\*\'\(\)]+$')
+
+        if not value.lower().endswith(('.jpg', '.png')):
+            raise ValidationError("Unsupported image file extension.")
+        if not re.search(pattern, value):
+            raise ValidationError("Incorrect image filename format.")
+
+        return value
+
+    def validate_permalink(self, value):
+        pattern = re.compile('^[a-z0-9\-\_]+$')
+
+        if not any(c.isalpha() for c in value):
+            raise ValidationError("Permalink must contain at least one alphabetic character.")
+        if not re.search(pattern, value):
+            raise ValidationError("Only lowercase letters/numbers/_/- are allowed in permalink.")
+
+        return value
+
+    def validate(self, data):
+        audio_filename = data.pop('audio_filename', None)
+        image_filename = data.pop('image_filename', None)
+        permalink = data.get('permalink')
+        
+        user = self.context.get('request').user
+        data['artist'] = user
+
+        detail = {}
+        track = self.instance
+
+        if audio_filename is not None:
+            audio_url = settings.S3_BASE_URL + settings.S3_MUSIC_TRACK_DIR + audio_filename
+            if (track is None or getattr(track, 'audio') != audio_url) and Track.objects.filter(audio=audio_url).exists():
+                detail['audio_filename'] = "Already existing audio file name."
+            data['audio'] = audio_url
+
+        if image_filename is not None:
+            image_url = settings.S3_BASE_URL + settings.S3_IMAGES_TRACK_DIR + image_filename
+            if (track is None or getattr(track, 'image') != image_url) and Track.objects.filter(image=image_url).exists():
+                detail['image_filename'] = "Already existing image file name."
+            data['image'] = image_url
+
+        if permalink is not None:
+            if (track is None or getattr(track, 'permalink') != permalink) and user.owned_tracks.filter(permalink=permalink).exists():
+                detail['permalink'] = "Already existing permalink for the user."
+
+        if any(detail):
+            raise ConflictError(detail)
+
+        return data
 
 
 class TrackUploadSerializer(TrackSerializer):
@@ -117,79 +185,6 @@ class TrackUploadSerializer(TrackSerializer):
             image_presigned_url = None
 
         return image_presigned_url
-
-    def validate(self, data):
-        user = self.context.get('request').user
-        audio_filename = data.get('audio_filename')
-        image_filename = data.get('image_filename')
-        permalink = data.get('permalink')
-        filename_pattern = re.compile('^[a-zA-Z0-9\/\!\-\_\.\*\'\(\)]+$')
-        permalink_pattern = re.compile('^[a-z0-9\-\_]+$')
-
-        detail = dict()
-
-        if audio_filename is not None:
-            if not audio_filename.lower().endswith(('.wav', '.flac', '.aiff', '.alac', 'mp3', '.aac', '.ogg', '.oga', '.mp4', '.mp2', '.m4a', '.3gp', '.3g2', '.mj2', '.amr', '.wma')):
-                detail['audio_filename_extension'] = "Unsupported audio file extension."
-            if not re.search(filename_pattern, audio_filename):
-                detail['audio_filename_pattern'] = "Incorrect audio filename format."
-
-        if image_filename is not None:
-            if not image_filename.lower().endswith(('.jpg', '.png')):
-                detail['image_filename_extension'] = "Unsupported image file extension."
-            if not re.search(filename_pattern, image_filename):
-                detail['image_filename_pattern'] = "Incorrect image filename format."
-
-        if permalink is not None:
-            if len(permalink) < 3 or len(permalink) > 255:
-                detail['permalink_length'] = "Permalink must be at least 3 characters and at most 255 characters."
-            if not any(c.isalpha() for c in permalink):
-                detail['permalink_alphabet'] = "Permalink must contain at least one alphabetic character."
-            if not re.search(permalink_pattern, permalink):
-                detail['permalink_pattern'] = "Only lowercase letters/numbers/_/- are allowed in permalink."
-
-        if detail:
-            raise ValidationError(detail)
-
-        # Check if the same filename (url) already exists in database.
-        detail = dict()
-
-        audio_url = settings.S3_BASE_URL + settings.S3_MUSIC_TRACK_DIR + audio_filename
-        if Track.objects.filter(audio=audio_url).exists():
-            detail['audio_filename'] = "Already existing audio file name."
-
-        if image_filename is not None:
-            image_url = settings.S3_BASE_URL + settings.S3_IMAGES_TRACK_DIR + image_filename
-            if Track.objects.filter(image=image_url).exists():
-                detail['image_filename'] = "Already existing image file name."
-
-        # Check if the same permalink already exists in database.
-        if user.owned_tracks.filter(permalink=permalink).exists():
-            detail['permalink'] = "Already existing permalink."
-
-        if detail:
-            raise ConflictError(detail)
-
-        return data
-
-    def create(self, validated_data):
-        user = self.context.get('request').user
-        audio_filename = validated_data.pop('audio_filename')
-        image_filename = validated_data.pop('image_filename', None)
-
-        audio_url = settings.S3_BASE_URL + settings.S3_MUSIC_TRACK_DIR + audio_filename
-        image_url = settings.S3_BASE_URL + settings.S3_IMAGES_TRACK_DIR + image_filename \
-            if image_filename is not None \
-            else None
-
-        validated_data.update(
-            {'artist': user, 'audio': audio_url, 'image': image_url}
-        )
-
-        track = super().create(validated_data)
-        data = TrackUploadSerializer(track).data
-
-        return data, status.HTTP_201_CREATED
 
 
 class SimpleTrackSerializer(serializers.ModelSerializer):
