53cdb6d1b2734daa446fca32b030189fa83b68b9
askrid <joonwoo3023@gmail.com>
2021-12-28 17:41:39 +0900

    Feat: schema

--- soundcloud/track/views.py
+++ soundcloud/track/views.py
@@ -1,35 +1,78 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 from track.models import Track
 from track.serializers import SimpleTrackSerializer, TrackSerializer, TrackUploadSerializer
-from drf_spectacular.utils import OpenApiResponse, extend_schema
+from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 
 
-class TrackViewSet(viewsets.GenericViewSet):
+@extend_schema_view(
+    create=extend_schema(
+        summary="Create Track",
+        responses={
+            '201': OpenApiResponse(response=TrackUploadSerializer, description='Created'),
+            '400': OpenApiResponse(description='Bad Request'),
+            '401': OpenApiResponse(description='Unauthorized'),
+            '409': OpenApiResponse(description='Conflict'),
+        }
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve Track",
+        responses={
+            '200': OpenApiResponse(response=TrackSerializer, description='OK'),
+            '404': OpenApiResponse(description='Not Found')
+        }
+    ),
+    update=extend_schema(
+        summary="Update Track",
+        responses={
+            '200': OpenApiResponse(response=TrackUploadSerializer, description='OK'),
+            '400': OpenApiResponse(description='Bad Request'),
+            '401': OpenApiResponse(description='Unauthorized'),
+            '403': OpenApiResponse(description='Permission Denied'),
+            '404': OpenApiResponse(description='Not Found'),
+            '409': OpenApiResponse(description='Conflict'),
+        }
+    ),
+    partial_update=extend_schema(
+        summary="Partial Update Track",
+        responses={
+            '200': OpenApiResponse(response=TrackUploadSerializer, description='OK'),
+            '400': OpenApiResponse(description='Bad Request'),
+            '401': OpenApiResponse(description='Unauthorized'),
+            '403': OpenApiResponse(description='Permission Denied'),
+            '404': OpenApiResponse(description='Not Found'),
+            '409': OpenApiResponse(description='Conflict'),
+        }
+    ),
+    destroy=extend_schema(
+        summary="Delete Track",
+        responses={
+            '204': OpenApiResponse(description='No Content'),
+            '401': OpenApiResponse(description='Unauthorized'),
+            '403': OpenApiResponse(description='Permission Denied'),
+            '404': OpenApiResponse(description='Not Found'),
+        }
+    ),
+    list=extend_schema(
+        summary="List Tracks",
+        responses={
+            '200': OpenApiResponse(response=TrackSerializer, description='OK'),
+        }
+    )
+)
+class TrackViewSet(viewsets.ModelViewSet):
 
-    query_set = Track.objects.all()
-    lookup_field = 'track_id'
+    queryset = Track.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'track_id'
 
     def get_serializer_class(self):
-        if self.action in ['create', 'update']:
+        if self.action in ['retrieve', 'delete']:
+            return TrackSerializer
+        elif self.action in ['create', 'update', 'partial_update']:
             return TrackUploadSerializer
         elif self.action in ['list']:
             return SimpleTrackSerializer
-        else:
-            return TrackSerializer
 
-    @extend_schema(
-        summary="Create Track",
-        responses={
-            201: OpenApiResponse(response=TrackUploadSerializer, description='Created'),
-            400: OpenApiResponse(description='Bad Request'),
-            401: OpenApiResponse(description='Unauthorized'),
-            409: OpenApiResponse(description='Conflict'),
-        }
-    )
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data, status = serializer.save()
 
-        return Response(data=data, status=status)
+    # TODO: 자기 트랙만 수정하게
+    # TODO: 409 에러 핸들링
