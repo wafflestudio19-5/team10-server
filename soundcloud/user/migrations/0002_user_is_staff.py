ac94e73c906bf91f2f95dae92a2ead8f28c9944d
askrid <joonwoo3023@gmail.com>
2022-01-01 23:37:59 +0900

    Fix: migration dependencies

--- soundcloud/user/migrations/0002_user_is_staff.py
+++ soundcloud/user/migrations/0002_user_is_staff.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0013_user_is_active'),
+        ('user', '0001_initial'),
     ]
 
     operations = [
