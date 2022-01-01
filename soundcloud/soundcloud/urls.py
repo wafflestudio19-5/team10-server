bda9062e6c304c69b07cae8ed38d99b0a269ef3c
askrid <joonwoo3023@gmail.com>
2022-01-01 23:41:55 +0900

    Fix: add comment.urls to urlpatterns (must be above the track.urls)

--- soundcloud/soundcloud/urls.py
+++ soundcloud/soundcloud/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('docs/swagger', SpectacularSwaggerView.as_view(url_name='schema-json'), name='swagger-ui'),
     path('docs/redoc', SpectacularRedocView.as_view(url_name='schema-json'), name='redoc'),
     path('', include('user.urls')),
+    path('', include('comment.urls')),
     path('', include('track.urls')),
     path('', include('reaction.urls')),
     path('', include('utility.urls')),
