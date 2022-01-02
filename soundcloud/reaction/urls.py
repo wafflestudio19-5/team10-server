78bf26cd976e4e2760689d9761d045b774ebecdb
bsm2002 <gggshhsl@gmail.com>
2022-01-03 03:26:58 +0900

    Feat: track reaction user list

--- soundcloud/reaction/urls.py
+++ soundcloud/reaction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LikeTrackView, LikeSetView, RepostTrackView, RepostSetView
+from .views import LikeTrackView, LikeSetView, RepostTrackView, RepostSetView, LikeView, RepostView
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('likes/sets/<str:set_id>', LikeSetView.as_view(), name='like-set'),
     path('reposts/tracks/<str:track_id>', RepostTrackView.as_view(), name='repost-track'),  # /reposts
     path('reposts/sets/<str:set_id>', RepostSetView.as_view(), name='repost-set'),
+    path('tracks/<int:track_id>/likers', LikeView.as_view(), name='likers'),
+    path('tracks/<int:track_id>/reposters', RepostView.as_view(), name='reposters'),
 ]
--- soundcloud/reaction/views.py
+++ soundcloud/reaction/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import GenericAPIView, CreateAPIView
+from rest_framework.generics import GenericAPIView, CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from set.models import Set
 from track.models import Track
+from user.models import User
+from reaction.models import Like, Repost
 from .serializers import LikeService, RepostService
+from user.serializers import SimpleUserSerializer
+from django.shortcuts import get_object_or_404
 
 
 class BaseReactionView(GenericAPIView):
@@ -57,3 +61,46 @@ class RepostSetView(BaseReactionView):
     serializer_class = RepostService
     queryset = Set.objects.all()
     lookup_url_kwarg = 'set_id'
+
+
+class BaseListView(ListAPIView):
+
+    serializer_class = None
+    queryset = None
+    lookup_field = 'id'
+    lookup_url_kwarg = None
+
+    def get_serializer_context(self):
+        context = super(BaseListView, self).get_serializer_context()
+        context['target'] = self.get_object()
+
+        return context
+
+
+class LikeView(BaseListView):
+
+    serializer_class = SimpleUserSerializer
+    lookup_url_kwarg = 'track_id'
+
+    def get_queryset(self):
+        filter_kwargs = {self.lookup_field: self.kwargs[self.lookup_url_kwarg]}
+        track = get_object_or_404(Track.objects.all(), **filter_kwargs)
+
+        like_qs = Like.objects.filter(object_id=track.id).values('id')
+        queryset = User.objects.filter(id__in=like_qs)
+
+        return queryset
+
+
+class RepostView(BaseListView):
+    serializer_class = SimpleUserSerializer
+    lookup_url_kwarg = 'track_id'
+
+    def get_queryset(self):
+        filter_kwargs = {self.lookup_field: self.kwargs[self.lookup_url_kwarg]}
+        track = get_object_or_404(Track.objects.all(), **filter_kwargs)
+
+        repost_qs = Repost.objects.filter(object_id=track.id).values('id')
+        queryset = User.objects.filter(id__in=repost_qs)
+
+        return queryset
