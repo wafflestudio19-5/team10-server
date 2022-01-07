d8adc20132fa5b8624c0e006b14dc0d917a167cd
ksiyeon27 <siyeonk27@gmail.com>
2022-01-07 15:06:44 +0900

    Fix: remove trailing slash

--- soundcloud/user/urls.py
+++ soundcloud/user/urls.py
@@ -7,9 +7,9 @@ router = SimpleRouter(trailing_slash=False)
 router.register('users', UserViewSet, basename='users')         # /users
 
 urlpatterns = [
-    path('signup/', UserSignUpView.as_view(), name='signup'),    # /signup
-    path('login/', UserLoginView.as_view(), name='login'),       # /login
-    path('logout/', UserLogoutView.as_view(), name='logout'),    # /logout
+    path('signup', UserSignUpView.as_view(), name='signup'),    # /signup
+    path('login', UserLoginView.as_view(), name='login'),       # /login
+    path('logout', UserLogoutView.as_view(), name='logout'),    # /logout
     path('users/me/followings/<int:user_id>', UserFollowView.as_view(), name='user-follow'),  # /users/me/followings/{user_id}
     path('users/me', UserSelfView.as_view(), name='user-self'), # /users/me
     path('', include(router.urls), name='user'),                # /users/{user_id}
