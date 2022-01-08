97b388258fc53d5d06e8d80016aa905193dc75d3
Choi Joonwoo <joonwoo3023@gmail.com>
2022-01-08 18:22:32 +0900

    Fix: remove unnecessary auth backend, which does not validate user password.

--- soundcloud/soundcloud/settings/common.py
+++ soundcloud/soundcloud/settings/common.py
@@ -178,7 +178,7 @@ REST_FRAMEWORK = {
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # this is default
     'guardian.backends.ObjectPermissionBackend',
-    'user.googleapi.GoogleBackend', 
+    # 'user.googleapi.GoogleBackend',   
 )
 
 # JWT Authorization
