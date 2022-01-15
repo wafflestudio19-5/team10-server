c68a809e1864b49110748165efad5d95da0a57e9
SiyeonKim <siyeonk27@gmail.com>
2022-01-15 23:53:06 +0900

    Comment: remove comments

--- soundcloud/set/views.py
+++ soundcloud/set/views.py
@@ -92,8 +92,6 @@ class SetViewSet(viewsets.ModelViewSet):
             return SetMediaUploadSerializer
         if self.action in ['likers', 'reposters']:
             return SimpleUserSerializer
-        if self.action in ['tracks']:
-            return SetTrackService
         else:
             return SetSerializer
 
