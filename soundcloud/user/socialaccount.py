dbfdb5f755ef23e4428f26bdec2497274d38f4aa
ksiyeon27 <siyeonk27@gmail.com>
2022-01-18 19:36:12 +0900

    Fix: google/callback -> /socialaccount & SocialAccountApi

--- soundcloud/user/socialaccount.py
+++ soundcloud/user/socialaccount.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from django.conf import settings
 from django.shortcuts import redirect
-from user.services import google_get_access_token, google_get_user_info
 from user.views import *
 from soundcloud.settings.common import *
 from django.db import transaction
@@ -14,28 +13,10 @@ from django.contrib.auth.backends import ModelBackend
 
 User = get_user_model()
 
-class GoogleLoginApi(APIView):
-    @extend_schema(
-        summary="Google Login",
-        tags=['auth', ],
-        responses={
-            200: OpenApiResponse(description='OK'),
-            400: OpenApiResponse(description='Bad Request'),
-        }
-    )
-    def get(self, request, *args, **kwargs): 
-        client_id = get_secret("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-        scope = "https://www.googleapis.com/auth/userinfo.email " + \
-                "https://www.googleapis.com/auth/userinfo.profile" 
-        redirect_uri = settings.BASE_BACKEND_URL + "/google/callback" 
-        google_auth_api = "https://accounts.google.com/o/oauth2/v2/auth" 
-        url = f"{google_auth_api}?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}" 
-        return Response(data={"url":url}, status=status.HTTP_200_OK)
-
-class GoogleSigninCallBackApi(APIView):
+class SocialAccountApi(APIView):
     permission_classes = (permissions.AllowAny, )
     def social_user_login(self, user, data): #jwt_login()
-        login(self.request, user, 'user.googleapi.GoogleBackend')  # GoogleBackend 를 통한 인증 시도
+        login(self.request, user, 'user.socialaccount.GoogleBackend')  # GoogleBackend 를 통한 인증 시도
         serializer = UserSocialLoginSerializer(user, data=data)
         serializer.is_valid(raise_exception=True)
         serializer.execute()
@@ -46,7 +27,7 @@ class GoogleSigninCallBackApi(APIView):
     def social_user_create(self, email, **extra_fields):
         data = copy.deepcopy(extra_fields)
         data["email"]=email
-        data["password"]=settings.GOOGLE_PASSWORD
+        data["password"]=settings.SOCIAL_PASSWORD
 
         serializer = UserCreateSerializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -68,7 +49,7 @@ class GoogleSigninCallBackApi(APIView):
 
 
     @extend_schema(
-        summary="Google Login Callback",
+        summary="Social User Signup/Login",
         tags=['auth', ],
         responses={
             200: OpenApiResponse(response=UserSocialLoginSerializer, description='OK'),
@@ -76,10 +57,6 @@ class GoogleSigninCallBackApi(APIView):
         }
     )
     def put(self, request, *args, **kwargs): 
-        # code = request.GET.get('code') 
-        # google_token_api = "https://oauth2.googleapis.com/token" 
-        # access_token = google_get_access_token(google_token_api, code) 
-        # user_data = google_get_user_info(access_token=access_token) # services.py method
         user_data = request.data.copy()
         profile_data = { 
             'email': user_data['email'],  #username->email
