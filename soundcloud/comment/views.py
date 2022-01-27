77c87659da97a6f373445ad55484852cef6bc437
askrid <joonwoo3023@gmail.com>
2022-01-27 22:24:10 +0900

    Feat: hide private tracks in CommentViewSet

--- soundcloud/comment/views.py
+++ soundcloud/comment/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets, mixins
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import get_object_or_404
@@ -16,28 +17,36 @@ class CommentViewSet(mixins.CreateModelMixin,
 
     serializer_class = TrackCommentSerializer
     permission_classes = (CustomObjectPermissions, )
-    lookup_field = 'id'
     lookup_url_kwarg = 'comment_id'
     filter_backends = (OrderingFilter, )
     ordering_fields = []
     ordering = ['-group__created_at', 'created_at']
 
     def get_queryset(self):
-        self.track = getattr(self, 'track', None) or get_object_or_404(Track, id=self.kwargs['track_id'])
+        
+        # hide private tracks in the queryset
+        user = self.request.user if self.request.user.is_authenticated else None
+        track_queryset = Track.objects.exclude(~Q(artist=user) & Q(is_private=True))
+
+        self.track = getattr(self, 'track', None) or get_object_or_404(track_queryset, id=self.kwargs['track_id'])
 
         if self.action in ['list']:
-            queryset = Comment.objects\
+            res = Comment.objects\
                 .select_related('writer')\
                 .prefetch_related('writer__followers', 'writer__owned_tracks')\
                 .filter(track=self.track)
         else:
-            queryset = Comment.objects.filter(track=self.track)
+            res = Comment.objects.filter(track=self.track)
 
-        return queryset
+        return res
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context['track'] = getattr(self, 'track', None) or get_object_or_404(Track, id=self.kwargs['track_id'])
+
+        # hide private tracks in the queryset
+        user = self.request.user if self.request.user.is_authenticated else None
+        track_queryset = Track.objects.exclude(~Q(artist=user) & Q(is_private=True))
+        context['track'] = getattr(self, 'track', None) or get_object_or_404(track_queryset, id=self.kwargs['track_id'])
 
         return context
 
